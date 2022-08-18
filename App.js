dd2632e32abecb95762567a31a9331c89e5d225d
romadebrian <roma_coll04@yahoo.com>
2022-08-18 11:35:07 +0700

    solve another version

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import React, {useEffect, useState} from 'react';
 import {app} from './src/config/firebase';
 import {getStorage, ref, uploadBytes, uploadString} from 'firebase/storage';
 
-// import ImagePicker, {launchImageLibrary} from 'react-native-image-picker';
-import * as ImagePicker from 'react-native-image-picker';
+import ImagePicker, {launchImageLibrary} from 'react-native-image-picker';
+// import * as ImagePicker from 'react-native-image-picker';
 import {async} from '@firebase/util';
 
 const App = () => {
@@ -15,57 +15,59 @@ const App = () => {
 
   const [valBase64, setValBase64] = useState(null);
 
-  useEffect(() => {
-    console.log(valBase64);
-  });
+  useEffect(() => {});
 
-  // const selectImage = () => {
-  //   const options = {
-  //     maxWidth: 2000,
-  //     maxHeight: 2000,
-  //     storageOptions: {
-  //       skipBackup: true,
-  //       path: 'images',
-  //     },
-  //     includeBase64: true,
-  //   };
+  const selectImage = async () => {
+    const options = {
+      maxWidth: 2000,
+      maxHeight: 2000,
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+      includeBase64: true,
+    };
 
-  //   launchImageLibrary(options, response => {
-  //     if (response.didCancel) {
-  //       console.log('User cancelled image picker');
-  //     } else if (response.error) {
-  //       console.log('ImagePicker Error: ', response.error);
-  //     } else if (response.customButton) {
-  //       console.log('User tapped custom button: ', response.customButton);
-  //     } else {
-  //       const source = {uri: response.assets[0].uri};
-  //       const filenya = response.assets[0].base64;
-  //       console.log(source);
-  //       setImage(source);
-  //       setValBase64(response.assets[0].base64);
+    launchImageLibrary(options, async response => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = {uri: response.assets[0].uri};
+        const filenya = response.assets[0].base64;
+        // console.log(source);
+        setImage(source);
+        setValBase64(response.assets[0].base64);
 
-  //       const storage = getStorage(app);
-  //       const storageRef = ref(storage, 'test.jpeg');
+        const storage = getStorage(app);
+        const storageRef = ref(storage, 'test.jpeg');
 
-  //       // 'file' comes from the Blob or File API
-  //       // uploadBytes(storageRef, filenya).then(snapshot => {
-  //       //   console.log('Uploaded a blob or file!');
-  //       // });
+        const img = await fetch(response.assets[0].uri);
+        const bytes = await img.blob();
 
-  //       // Base64 formatted string
-  //       // const message2 = response.assets[0].base64;
-  //       // uploadString(storageRef, message2, 'base64').then(snapshot => {
-  //       //   console.log('Uploaded a base64 string!', snapshot);
-  //       // });
+        // 'file' comes from the Blob or File API
+        uploadBytes(storageRef, bytes).then(snapshot => {
+          console.log('Uploaded a blob or file!', snapshot);
+          console.log('Uploaded a blob or file!', bytes);
+        });
 
-  //       // Data URL string
-  //       const message4 = `data:image/jpeg;base64,${filenya}`;
-  //       uploadString(storageRef, message4, 'data_url').then(snapshot => {
-  //         console.log('Uploaded a data_url string!', snapshot);
-  //       });
-  //     }
-  //   });
-  // };
+        // Base64 formatted string
+        // const message2 = response.assets[0].base64;
+        // uploadString(storageRef, message2, 'base64').then(snapshot => {
+        //   console.log('Uploaded a base64 string!', snapshot);
+        // });
+
+        // Data URL string
+        // const message4 = `data:image/jpeg;base64,${filenya}`;
+        // uploadString(storageRef, message4, 'data_url').then(snapshot => {
+        //   console.log('Uploaded a data_url string!', snapshot);
+        // });
+      }
+    });
+  };
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibrary({
@@ -117,7 +119,7 @@ const App = () => {
       )}
 
       <TouchableOpacity
-        onPress={pickImage}
+        onPress={selectImage}
         style={{
           width: 100,
           backgroundColor: '#e3d4d3',
